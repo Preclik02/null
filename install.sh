b7941014bd57e3a8765fe49b2dab723b47152085
Preclik02 <ransdorfd@gmail.com>
2026-06-17 12:49:31 +0200

    small edit

--- install.sh
+++ install.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# --- System Paths ---
 TARGET_DIR="/usr/local/lib/null"
 BIN_DIR="/usr/local/bin"
 
-# Ensure the script is run as root
 if [ "$EUID" -ne 0 ]; then
-    echo "[-] Please run as root (sudo)."
+    echo "[+] Please run as root (sudo)."
     exit 1
 fi
 
-# Ensure it is running on an Arch-based system
 if [ ! -f /etc/arch-release ]; then
-    echo "[-] Error: This installer is strictly optimized for Arch-based distributions."
+    echo "[+] Error: This installer is strictly optimized for Arch-based distributions."
     exit 1
 fi
 
 echo "[+] Installing core build system packages..."
-# Only installing make, sl, openssh, figlet, and sshfs now.
 pacman -Syu --needed --noconfirm make sl openssh figlet sshfs
 
 echo "[+] Cleaning up any previous installations..."
